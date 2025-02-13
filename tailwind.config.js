66a0c32074681c81849b05f47bcf930bb74aa373
phuhuy11 <phuhuy11@innlandetfylke.no>
2025-02-13 14:10:13 +0100

    fix for Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: ["class"],
-    content: [],
+    content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/app/**/*.{js,ts,jsx,tsx}",
+	],
   theme: {
   	extend: {
   		borderRadius: {
