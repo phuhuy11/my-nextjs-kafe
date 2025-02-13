30f4a7af654854e71ec9f59069cfb30726cddb03
phuhuy11 <phuhuy11@innlandetfylke.no>
2025-02-13 14:42:14 +0100

    make logo white

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
+          className="invert dark:invert-0"
           src="/next.svg"
           alt="Next.js logo"
           width={180}
