870b32d4999d0ff1db427871732695bbc698144a
phuhuy11 <phuhuy11@innlandetfylke.no>
2025-02-17 09:32:59 +0100

    more text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Then you can rest.</li>
+          <li>Then you can change whatever you like.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
